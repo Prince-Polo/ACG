58cc8a534702bebb51a58a7ccadd28e0daf154f2
杨承翰 <ch-yang23@mails.tsinghua.edu.cn>
2025-01-06 23:49:34 +0800

    Update iisph_container.py

--- SPH/containers/iisph_container.py
+++ SPH/containers/iisph_container.py
@@ -9,11 +9,12 @@ class IISPHContainer(BaseContainer):
         self.density_error = ti.field(dtype=float, shape=())
         self.iisph_a_ii = ti.field(dtype=ti.f32, shape=self.particle_max_num)
         self.iisph_source = ti.field(dtype=ti.f32, shape=self.particle_max_num)
-        self.iisph_pressure_accelerations = ti.Vector.field(self.dim, dtype=float, shape=self.particle_max_num)
-        self.iisph_pressure = ti.field(dtype=ti.f32, shape=self.particle_max_num)
-        self.iisph_laplacian = ti.field(dtype=ti.f32, shape=self.particle_max_num)    
-        self.iisph_sum_dij = ti.Vector.field(self.dim, dtype=ti.f32, shape=self.particle_max_num)
         
         self.max_iterations = 2000
         self.eta = 0.001 # This criterion is given by our reference paper
         self.omega = 0.08
+        
+        self.iisph_pressure_accelerations = ti.Vector.field(self.dim, dtype=float, shape=self.particle_max_num)
+        self.iisph_pressure = ti.field(dtype=ti.f32, shape=self.particle_max_num)
+        self.iisph_laplacian = ti.field(dtype=ti.f32, shape=self.particle_max_num)    
+        self.iisph_sum_dij = ti.Vector.field(self.dim, dtype=ti.f32, shape=self.particle_max_num)
